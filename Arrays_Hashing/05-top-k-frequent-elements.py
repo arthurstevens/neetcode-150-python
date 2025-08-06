88a1c358c19226239932060df987ad30e13ea6b8
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-08-06 13:15:56 +0100

    fix(arrays_hashing): prevent shared reference bug in top k frequent elements

--- Arrays_Hashing/05-top-k-frequent-elements.py
+++ Arrays_Hashing/05-top-k-frequent-elements.py
@@ -12,7 +12,7 @@ class Solution:
         for num in nums:
             freq_map[num] = 1 + freq_map.get(num, 0)
         
-        buckets = [[] for i in range (len(nums) + 1)]
+        buckets = [[] for _ in range (len(nums) + 1)]
         for val, freq in freq_map.items():
             buckets[freq].append(val)
             
