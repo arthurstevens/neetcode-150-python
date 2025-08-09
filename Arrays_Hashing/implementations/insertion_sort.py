0371b1b7d3d42d2b69bead64d100faa1e8b5217c
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-08-09 11:07:29 +0100

    feat(arrays_hashing): implement insertion sort

---
+++ Arrays_Hashing/implementations/insertion_sort.py
@@ -0,0 +1,21 @@
+# Implementation of insertion sort
+# Time complexity: O()
+# Space complexity: O()
+
+from typing import List
+
+
+def insertionSort(arr: List[int]) -> List[int]:
+    count = len(arr)
+    if count <= 1:
+        return arr
+    
+    for i in range(1, count):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        
+    return arr
