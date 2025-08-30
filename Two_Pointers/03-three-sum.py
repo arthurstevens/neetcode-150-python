597d3b87978d0a005af5a64c02099dd968767efe
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-08-30 21:54:00 +0100

    feat(two_pointers): add three_sum

---
+++ Two_Pointers/03-three-sum.py
@@ -0,0 +1,32 @@
+# Problem: https://leetcode.com/problems/3sum/
+# NeetCode 150 – Two Pointers
+# Difficulty: Medium
+# Time: O(n^2), Space: O(1)
+
+from typing import List
+
+
+class Solution:
+	def threeSum(self, nums: List[int]) -> List[List[int]]:
+		nums.sort()
+		triplets = []
+		
+		for i in range(len(nums)):
+			if i > 0 and nums[i] == nums[i-1]:
+				continue
+
+			j, k = i + 1, len(nums) - 1
+			while j < k:
+				sum = nums[i] + nums[j] + nums [k]
+				if sum < 0:
+					j += 1
+				elif sum > 0:
+					k -= 1
+				else:
+					triplets.append([nums[i], nums[j], nums [k]])
+
+					j += 1
+					while nums[j] == nums[j-1] and j < k:
+						j += 1
+
+		return triplets
