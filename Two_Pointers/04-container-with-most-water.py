ec3eabba9968c3c9c6cd5291c41c9bb5a06bf20b
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-08-31 20:14:30 +0100

    feat(two_pointers): fix container with most water

--- Two_Pointers/04-container-with-most-water.py
+++ Two_Pointers/04-container-with-most-water.py
@@ -1,7 +1,7 @@
 # Problem: https://leetcode.com/problems/container-with-most-water/
 # NeetCode 150 – Two Pointers
 # Difficulty: Medium
-# Time: O(), Space: O()
+# Time: O(n), Space: O(1)
 
 from typing import List
 
@@ -10,12 +10,10 @@ class Solution:
 	def maxArea(self, height: List[int]) -> int:
 		l, r = 0, len(height) - 1
 		max_vol = None
-
 		while l < r:
 			max_vol = max(min(height[l], height[r]) * (r - l), max_vol)
 			if height[l] > height[r]:
 				r -= 1
 			else:
 				l += 1
-
 		return max_vol
