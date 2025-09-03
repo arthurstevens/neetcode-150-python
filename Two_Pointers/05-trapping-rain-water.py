f67fb02428b5feec76355749c2dbd72fb116f41f
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-09-03 21:25:57 +0100

    feat(two_pointers): optimise trapping rain water

--- Two_Pointers/05-trapping-rain-water.py
+++ Two_Pointers/05-trapping-rain-water.py
@@ -1,45 +1,24 @@
 # Problem: https://leetcode.com/problems/trapping-rain-water/
 # NeetCode 150 – Two Pointers
 # Difficulty: Easy/Medium/Hard
-# Time: O(), Space: O()
+# Time: O(n), Space: O(1)
 
 from typing import List
 
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        total_area = 0
-        i = 0
-        while i < len(height) - 1:
-            if height[i] == 0:
-                i += 1
-                continue
-
-            cur_wall = height[i]
-            j = i + 1
-            max_wall = (j, 0)
-
-            while j < len(height) and height[j] < cur_wall:
-                if height[j] > max_wall[1]:
-                    max_wall = (j, height[j])
-                j += 1
-
-            if j < len(height):
-                wall_idx = j
-                wall_height = height[j]
+        trapped_water = 0
+        l, r = 0, len(height) - 1
+        l_max, r_max = 0, 0
+        while l <= r:
+            if height[l] < height[r]:
+                l_max = max(l_max, height[l])
+                trapped_water += l_max - height[l]
+                l += 1
             else:
-                wall_idx = max_wall[0]
-                wall_height = max_wall[1]
-				
-            min_height = min(cur_wall, wall_height)
-            for k in range(i + 1, wall_idx):
-                total_area += max(0, min_height - height[k])
+                r_max = max(r_max, height[r])
+                trapped_water += r_max - height[r]
+                r -= 1
 
-            i = wall_idx  # Move to next wall
-
-        return total_area
-
-
-ls = [0,1,0,2,1,0,1,3,2,1,2,1]
-o = Solution()
-o.trap
+        return trapped_water
