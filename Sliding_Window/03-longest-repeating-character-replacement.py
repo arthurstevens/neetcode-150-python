b1735d9236838c1a7322c9e7ee83afba7df3eccf
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-09-12 21:42:34 +0100

    feat(two_pointers): optimise longest repeating character replacement

--- Sliding_Window/03-longest-repeating-character-replacement.py
+++ Sliding_Window/03-longest-repeating-character-replacement.py
@@ -1,18 +1,18 @@
 # Problem: https://leetcode.com/problems/longest-repeating-character-replacement/
 # NeetCode 150 – Sliding Window
 # Difficulty: Medium
-# Time: O(), Space: O()
+# Time: O(n), Space: O(1)
 
 class Solution:
 	def characterReplacement(self, s: str, k: int) -> int:
-		l = 0
-		maxL = 0
-		maxF = 0
+		l = maxL = maxF = 0
+		chrs_base = ord('A')
 		chrs = [0] * 26
 		for r in range(len(s)):
-			chrs[ord(s[r]) - ord('A')] += 1
-			maxF 
-			while r - l + 1 - max(chrs) > k:
+			i = ord(s[r]) - chrs_base
+			chrs[i] += 1
+			maxF = max(maxF, chrs[i])
+			while r - l + 1 - maxF > k:
 				chrs[ord(s[l]) - ord('A')] -= 1
 				l += 1
 			maxL = max(maxL, r - l + 1)
