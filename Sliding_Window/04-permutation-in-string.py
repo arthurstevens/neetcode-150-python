71b5d4af90b7b2b1f5b7601b3acabddc2e89bea2
Arthur Srodzinski-Stevens <arthurstevens228@gmail.com>
2025-09-14 12:17:41 +0100

    feat(sliding_window): optimise permutation in string

--- Sliding_Window/04-permutation-in-string.py
+++ Sliding_Window/04-permutation-in-string.py
@@ -1,8 +1,49 @@
 # Problem: https://leetcode.com/problems/permutation-in-string/
 # NeetCode 150 – Sliding Window
 # Difficulty: Medium
-# Time: O(), Space: O()
+# Time: O(n), Space: O(1)
 
+# Optimal solution utilising a matches counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        idx_base = ord('a')
+        s1_set = [0] * 26
+        window_set = [0] * 26
+
+        matches = 26
+        for c in s1:
+            i = ord(c) - idx_base
+            if s1_set[i] == 0:
+                matches -= 1
+            s1_set[i] += 1
+
+        l = 0
+        for r in range(len(s2)):
+            i = ord(s2[r]) - idx_base
+            if window_set[i] == s1_set[i]:
+                matches -= 1
+            elif window_set[i] + 1 == s1_set[i]:
+                matches += 1
+            window_set[i] += 1
+
+            if r - l + 1 > len(s1):
+                i = ord(s2[l]) - idx_base
+                if window_set[i] == s1_set[i]:
+                    matches -= 1
+                elif window_set[i] - 1 == s1_set[i]:
+                    matches += 1
+                window_set[i] -= 1
+                l += 1
+
+            if matches == 26:
+                return True
+
+        return False
+
+# Simpler solution for more readability, negligible performance difference but technically slower
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
@@ -15,27 +56,15 @@ class Solution:
         for c in s1:
             s1_set[ord(c) - idx_base] += 1
 
-        matches = s1_set.count(0)
-
         l = 0
         for r in range(len(s2)):
-            i = ord(s2[r]) - idx_base
-            if window_set[i] == s1_set[i]:
-                matches -= 1
-            window_set[i] += 1
-            if window_set[i] == s1_set[i]:
-                matches += 1
+            window_set[ord(s2[r]) - idx_base] += 1
 
             if r - l + 1 > len(s1):
-                i = ord(s2[l]) - idx_base
-                if window_set[i] == s1_set[i]:
-                    matches -= 1
-                window_set[i] -= 1
-                if window_set[i] == s1_set[i]:
-                    matches += 1
+                window_set[ord(s2[l]) - idx_base] -= 1
                 l += 1
 
-            if matches == 26:
+            if r - l + 1 == len(s1) and window_set == s1_set:
                 return True
 
         return False
