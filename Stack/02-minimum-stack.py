d6dde3c39093ed9ace7e13693741d168aecb19de
Custord <126922564+CustordSolos@users.noreply.github.com>
2025-10-04 23:38:42 +0100

    feat(stack): add one-stack solution to minimum stack

--- Stack/02-minimum-stack.py
+++ Stack/02-minimum-stack.py
@@ -3,6 +3,10 @@
 # Difficulty: Medium
 # Time: O(1) for all operations, Space: O(n)
 
+from typing import Optional
+
+
+# Two stacks
 class MinStack:
     def __init__(self):
         self.stack = []
@@ -22,3 +26,38 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stack[-1]
+    
+# One stack (higher memory efficiency)
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min = None
+
+    def push(self, val: int) -> None:
+        if self.min is None:
+            self.min = val
+            self.stack.append(0)
+        else:
+            self.stack.append(val - self.min)
+            self.min = min(val, self.min)
+
+    def pop(self) -> None:
+        if not self.stack: 
+            return
+        val = self.stack.pop()
+        if val < 0:
+            self.min -= val
+        if not self.stack:
+            self.min = None
+
+    def top(self) -> Optional[int]:
+        if self.min is None:
+            return None
+        val = self.stack[-1]
+        if val > 0:
+            return self.min + val
+        else:
+            return self.min
+        
+    def getMin(self) -> Optional[int]:
+        return self.min
